d0ae6352f319b20e075d869de592e9259bb0e380
IngsStools <72052186+IngsStools@users.noreply.github.com>
2020-09-29 07:08:52 +0700

    Installer Tools New 2020

---
+++ Indo-Tools.sh
@@ -0,0 +1,230 @@
+#!/system/xbin/bash
+
+#====================================================
+#Record lah Sepuas Kalian
+#Karna saya ga mau,, ilmu yang saya miliki
+#Berhenti di diri saya sendiri,, itu Sangat Sia Sia :)
+#Save No admin [ 089514334669 ]
+#====================================================
+
+clear
+xdg-open https://wa.wizard.id/b9c743
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○"
+echo "\033[35;1m ● \033[37;1mAuthor  \033[36;1m| \033[30;1mMr.IngsS"
+echo "\033[36;1m ● \033[37;1mRegion  \033[36;1m| \033[31;1mIndon\033[37;1mesian"
+echo "\033[32;1m ● \033[37;1mGithub  \033[36;1m| \033[30;1mhttps://github.com/HostIngsS"
+echo "\033[34;1m ● \033[37;1mContact \033[36;1m| \033[30;1mSecnet@mbox.re"
+echo "\033[33;1m○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○○ \033[37;1m[ \033[35;1mV.01 \033[37;1m]"
+echo ""
+echo "\033[36;1m ● \033[41;1m\033[37;1mINSTALLING BAHAN TERLEBIH DAHULU\033[00;1m \033[37;1m[ \033[32;1m● \033[36;1m2020 \033[37;1m]"
+echo "\033[00;1m"
+echo "\033[31;1m[ \033[37;1m01 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Facebook"
+echo "\033[31;1m[ \033[37;1m02 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Instagram"
+echo "\033[31;1m[ \033[37;1m03 \033[31;1m] \033[33;1m● \033[32;1mBruteforce File ZIP"
+echo "\033[31;1m[ \033[37;1m04 \033[31;1m] \033[33;1m● \033[32;1mBruteforce Gmail"
+echo "\033[31;1m[ \033[37;1m05 \033[31;1m] \033[33;1m● \033[32;1mSaydog-Framework"
+echo "\033[31;1m[ \033[37;1m06 \033[31;1m] \033[33;1m● \033[32;1mKeylogger Creator"
+echo "\033[31;1m[ \033[37;1m07 \033[31;1m] \033[33;1m● \033[32;1mAPK Email Fake"
+echo "\033[31;1m[ \033[37;1m08 \033[31;1m] \033[33;1m● \033[32;1mSMS Fake"
+echo "\033[31;1m[ \033[37;1m09 \033[31;1m] \033[33;1m● \033[32;1mCreate Virus Mallware"
+echo "\033[31;1m[ \033[37;1m10 \033[31;1m] \033[33;1m● \033[32;1mPishing Social Media"
+echo "\033[31;1m[ \033[37;1m11 \033[31;1m] \033[33;1m● \033[32;1mDummper Empas Moonton"
+echo "\033[31;1m[ \033[37;1m12 \033[31;1m] \033[33;1m● \033[32;1mSpam-Call Fake"
+echo "\033[31;1m[ \033[37;1m13 \033[31;1m] \033[33;1m● \033[41;1m\033[37;1mInstalling Bahan\033[00;1m"
+echo "\033[31;1m[ \033[37;1m14 \033[31;1m] \033[33;1m● \033[32;1mDownload Indonesian-Wordlist Password"
+echo "\033[31;1m[ \033[37;1m15 \033[31;1m] \033[33;1m● \033[32;1mExiting Program\033[36;1m"
+echo "\033[37;1m"
+read -p "Input Number : " pilih
+if [ $pilih = "01" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Facebook \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/HostIngsS/IngsBF.git
+    cd IngsBF
+    python2 Bruteings.py
+fi
+
+if [ $pilih = "02" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Instagram \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/Yv7r3hOBIFc
+    cd $HOME
+    git clone https://github.com/IL4NGQW3R/mbi
+    cd mbi
+    python mbi.py
+fi
+
+if [ $pilih = "03" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce File ZIP \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/shareef004/termux-brute-force-zip-crack-python
+    cd termux-brute-force-zip-crack-python
+    python cracker.py
+fi
+
+if [ $pilih = "04" ]
+then
+    clear
+    echo "\033[32;1mInstalling Bruteforce Gmail \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/EWmAWTj5Lgo
+    cd $HOME
+    git clone https://github.com/KINGTEBE-404/Gmail-Attack.git
+    cd Gmail-Attack
+    python2 attack.py
+fi
+
+if [ $pilih = "05" ]
+then
+    clear
+    echo "\033[32;1mInstalling Saydog-Framework \033[37;1m..."
+    sleep 2
+    xdg-open https://instagram.com/saydog.official?igshid=6mxrawk2w8yn
+    cd $HOME
+    git clone https://github.com/saydog-official/saydog-framework.git
+    cd saydog-framework
+    chmod +x *
+    pip install -r requirements.txt
+    bash setup
+fi
+
+if [ $pilih = "06" ]
+then
+    clear
+    echo "\033[32;1mInstalling Keylogger Creator \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/lJzUfWO1hQE
+    cd $HOME
+    git clone https://github.com/Zidan-ID17/Logger-X-Creator.git
+    cd Logger-X-Creator
+    python run.py
+fi
+
+if [ $pilih = "07" ]
+then
+     clear
+    echo "\033[32;1mInstalling Email Fake \033[37;1m..."
+    sleep 2
+    cd $HOME
+    xdg-open https://play.google.com/store/apps/details?id=air.kukulive.mailnow
+fi
+
+if [ $pilih = "08" ]
+then
+    clear
+    echo "\033[32;1mInstalling SMS Fake \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/f23ERcliqy8
+    cd $HOME
+    git clone https://github.com/B4N954N2-ID/spam-sms.git
+    cd spam-sms
+    pip install -r requirements.txt
+    python2 sms.py
+fi
+
+if [ $pilih = "09" ]
+then
+    clear
+    echo "\033[32;1mInstalling Create Virus Mallware \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/BlackCyberAnonim/B-VIRUS.git
+    cd B-VIRUS
+    python2 B-VIRUS.py
+fi
+
+if [ $pilih = "10" ]
+then
+    clear
+    echo "\033[32;1mInstalling Pishing Social Media \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/htr-tech/nexphisher
+    cd nexphisher
+    bash setup
+fi
+
+if [ $pilih = "11" ]
+then
+    clear
+    echo "\033[32;1mInstalling Dummper Empas Moonton \033[37;1m..."
+    sleep 2
+    xdg-open https://youtu.be/gWebMEU3wHk
+    cd $HOME
+    git clone https://github.com/limitqueenproject/empas-limit.git
+    cd empas-limit
+    bash empas
+fi
+
+if [ $pilih = "12" ]
+then
+    clear
+    echo "\033[32;1mInstalling Spam-Call Fake \033[37;1m..."
+    sleep 2
+    cd $HOME
+    git clone https://github.com/IL4NGQW3R/spammer
+    cd spammer
+    python spam-22.py
+fi
+
+if [ $pilih = "13" ]
+then
+    clear
+    echo "\033[32;1mInstalling Installing Bahan \033[37;1m..."
+    sleep 1
+    echo "\033[32;1m● \033[37;1mMohon Tunggu \033[37;1m..."
+    cd $HOME
+    pkg update && pkg upgrade
+    pkg install python -y
+    pkg install python2 -y
+    pkg install nano
+    pkg install wget
+    pkg install mc
+    pkg install curl
+    pkg install git
+    pkg install php
+    pip2 install requests
+    pip2 install tqdm
+    pip install tqdm
+    pip2 install yagmail
+    pip install yagmail
+    pip install requests
+    pip install --upgrade pip
+    pip2 install mechanize
+    pip install mechanize
+    pip2 install bs4
+    pip install bs4
+    pip2 install colorama
+    pip install colorama
+    python3 -m pip install bs4
+    python3 -m pip install requests
+    echo "\033[32;1m● \033[37;1mInstalling Bahan Success"
+    sleep 10
+    echo "\033[32;1m● \033[31;1mKetikan \033[30;1m: \033[37;1msh Indotools.sh"
+fi
+
+if [ $pilih = "14" ]
+then
+    clear
+    echo "\033[32;1mDownloading Indonesian-Wordlist-Password \033[37;1m..."
+    sleep 2
+    cd $HOME
+    xdg-open https://www.mediafire.com/download/x75aqcy1je5hm71
+fi
+
+if [ $pilih = "15" ]
+then
+    clear
+    echo "\033[32;1m● \033[37;1m Terima kasih telah menggunakan tools ini \033[31;1m..."
+    echo "\033[32;1m● \033[37;1m Jika ingin merecord tools ini, di mohon izin dahulu \033[31;1m..."
+    sleep 5
+    echo "\033[32;1m● \033[37;1m Exiting/Program \033[31;1m \033[00;1m..."
+    exit
+fi
